7128be62a6cd8ad48ee9d2c7acd47cf430abbcdd
Devhocke <71835476+DevHocke@users.noreply.github.com>
2021-10-06 17:15:50 +0200

    Started Methods, calculateUserInputCheckIfPrimeNumber, assUserInputToPrimeNumberList, printListOfPrimeNumbers and the private List primeNumbers.

--- PrimeMethods.cs
+++ PrimeMethods.cs
@@ -8,5 +8,25 @@ namespace Primtal
 {
     class PrimeMethods
     {
+        private List<int> primeNumbers = new List<int>();
+
+        public int calculateUserInputCheckIfPrimeNumber(float userInput)
+        {
+            return 0;
+        }
+
+        public int addUserInputToPrimeNumberList(float addNumberToList)
+        {
+            return 0;
+        }
+
+        public void printListOfPrimeNumbers(List<int> primeNumbers)
+        {
+            foreach (var number in primeNumbers)
+            {
+                Console.WriteLine(number);
+            }
+        }
+
     }
 }
